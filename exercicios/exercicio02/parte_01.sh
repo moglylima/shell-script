465140b11b093ffad185187440da9d2d6ca30424
MoglesonLima <moglesonlima@alu.ufc.br>
2020-12-08 22:01:12 -0300

    Envio do ecercicio, ate onde consegui

---
+++ exercicios/exercicio02/parte_01.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+
+
+grep -E '[[:space:]]A' /home/compartilhado/emailsordenados.txt
+
+grep -E '.br$' /home/compartilhado/emailsordenados.txt
+
+grep -E '[[:digit:]]@' /home/compartilhado/emailsordenados.txt
+
+
