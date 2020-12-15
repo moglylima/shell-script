c1ed2c921ee577d794998f629ec43f563b91ba50
MoglesonLima <moglesonlima@alu.ufc.br>
2020-12-15 19:01:53 -0300

    Envio Final, removendo partes desnecessárias

--- atividades/atividade03/logins.sh
+++ atividades/atividade03/logins.sh
@@ -7,7 +7,7 @@ grep -v 'sshd' /home/compartilhado/auth.log.1
 grep -E 'sshd[[[:digit:]]*]:[[:space:]]Accepted' /home/compartilhado/auth.log.1
 
 #Tentativas de Logins via root
-grep  'sshd[[[:digit:]]*]:[[:space:]]Connection closed by authenticating user root' /home/compartilhado/auth.log.1
+grep  'Connection closed by authenticating user root' /home/compartilhado/auth.log.1
 
 #Login com sucesso no dia 04 de dezembro, entre as 18:00 e 19:00
 grep -E 'Dec[[:space:]]*4[[:space:]]*(18.*|19:00.*).*sshd[[[:digit:]]*]:[[:space:]]Accepted' /home/compartilhado/auth.log.1
