309ddbf0dd833189dc49652916600eaeef283a7d
João Marcelo Uchôa de Alencar <jmarcelo.alencar@gmail.com>
2020-12-28 10:41:20 -0300

    Correção.

--- atividades/atividade04/atualizarDiretorios.sh
+++ atividades/atividade04/atualizarDiretorios.sh
@@ -1,4 +1,5 @@
 #!/bin/bash
+# Correção: 1,0
 
 #Criando arquivo passwd.new alterando o endereço da pasta de todos os alunos
 sed 's/home\/alunos/srv\/students/g' /etc/passwd > /home/alunos/moglesonlima/passwd.new 
