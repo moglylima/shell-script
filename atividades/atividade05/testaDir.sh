85c97dda48ed290ff3e087db418f242be20b0213
MoglesonLima <moglesonlima@alu.ufc.br>
2021-01-13 22:45:09 -0300

    Envio 03 atividade04

--- atividades/atividade05/testaDir.sh
+++ atividades/atividade05/testaDir.sh
@@ -1,12 +1,10 @@
 #!/bin/bash
 
 PARAMETRO=$1
-
-if [ -r ${PARAMETRO} ]
-then
-	QTDITENS= ls ${PARAMETRO} | wc -l
-	DIRSIZE= du -sk ${PARAMETRO} | cut -f1
-
+DIRSIZE= `du -sk ${PARAMETRO} | cut -f1` 
+QTDITENS= `ls -l ${PARAMETRO} | grep "^-" -c`
+if [ -d ${dir} ]
+then 
 	echo "O diretório ${PARAMETRO} ocupa ${DIRSIZE} kilobytes e tem ${QTDITENS} itens."
 else
 	echo "${PARAMETRO} não é um diretório!!!"
