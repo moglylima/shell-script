65d9d962ba9d9333d8b8bca6a8f88df1ea176239
MoglesonLima <moglesonlima@alu.ufc.br>
2021-02-02 16:00:44 -0300

    Envio 02, atividade 10, concluida

--- atividades/atividade10/calculadora.sh
+++ atividades/atividade10/calculadora.sh
@@ -24,33 +24,36 @@ do
    # Se já tiver alguma operação na tela, apague
    if [[ $VISOR =~ .*=.* ]]
    then
-      VISOR=
+	   VISOR=
    fi
 
    # se você digitou um número
    if [[ $TECLA =~ [0-9]+ ]]  
    then
-       # e ainda não tem nada na tela
-       if [ ! "$VISOR" ]
-       then
-          # coloque o número na tela
-          VISOR=$TECLA
-       else
-          # e já existe um número e um operador na tela
-          if [[ $VISOR =~ [0-9]+[[:blank:]][*/\+\-] ]]
-          then
-             # faça a operação
-             VISOR="$VISOR $TECLA = `expr $VISOR $TECLA`"
-          fi
-       fi
+      # e ainda não tem nada na tela
+      if [ ! "$VISOR" ]
+      then
+         # coloque o número na tela
+         VISOR=$TECLA
+      else
+         # e já existe um número e um operador na tela
+         if [[ $VISOR =~ [0-9]+[[:blank:]][*/\+\-] ]]
+	  then
+         #Alterando o valor da variável scale, para que se possa realizar operações com ponto flutuante
+         #sed ao final visa adição de um zero antes do ponto, em casos onde o resultado < 1
+         VISOR="$VISOR $TECLA = `echo "scale=2 ; $VISOR $TECLA" | bc  | sed "s/^[.]/0./"`"
+         echo $VISOR > res
+       
+	   fi
+         fi
    # se você não digitou um número
    else
        # mas digitou um operador
-       if [[ $TECLA =~ [*/\+\-] ]]
-       then
-          # atualize a tela para mostrar o primeiro número e o operador
-          VISOR="$VISOR $TECLA"
-       fi
+      if [[ $TECLA =~ [*/\+\-] ]]
+      then
+         # atualize a tela para mostrar o primeiro número e o operador
+         VISOR="$VISOR $TECLA"
+      fi
    fi
 
 done
