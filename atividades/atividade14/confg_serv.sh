2374d0520046c03adce72fa934841f0f2cadd83d
Mogleson Lima <57246109+MoglesonLima@users.noreply.github.com>
2021-03-04 00:08:57 -0300

    Update confg_serv.sh
    Apenas ajustes no caminho do index.html

--- atividades/atividade14/confg_serv.sh
+++ atividades/atividade14/confg_serv.sh
@@ -7,7 +7,7 @@ sudo apt install -y apache2
 sudo mv /var/www/html/index.html /var/www/html/index.html.bkp
 
 #Gerando novo Index, como nome e matricula
-sudo printf "<html lang="pt-br">\n  <head>\n    <title>Atividade 04 Programação de scripts</title>\n    <meta charset="utf-8">\n  </head>\n  <body>\n    <h2>Aluno: Mogleson Lima - Matricula: 472007<h2>\n  </body>\n</html>\n" > index.html
+sudo printf "<html lang="pt-br">\n  <head>\n    <title>Atividade 04 Programação de scripts</title>\n    <meta charset="utf-8">\n  </head>\n  <body>\n    <h2>Aluno: Mogleson Lima - Matricula: 472007<h2>\n  </body>\n</html>\n" > /var/www/html/index.html
 
 #Reinicializando apache2
 sudo systemctl restart apache2
