fb18c80c83a5bd8b392451652a1e378db3d25dbb
Mogleson Lima <moglesonlima@alu.ufc.br>
2021-03-11 00:58:56 -0300

    Atualizando

--- atividades/atividade15/confg_serv.sh
+++ atividades/atividade15/confg_serv.sh
@@ -9,5 +9,16 @@ mv /var/www/html/index.html /var/www/html/index.html.bkp
 #Gerando novo Index, para serem direcionados os relatórios
 printf "<html lang="pt-br">\n  <head>\n    <title>Atividade 15 Programação de scripts</title>\n    <meta charset="utf-8">\n  </head>\n  <body>\n    <h2>Realatório Servidor</h2>\n   <ol>\n Fim... \n</ol>    \n</body>\n</html>\n" > /var/www/html/index.html
 
+#Script serviço
+printf "" > /usr/local/bin/servico.sh
+
+chmod 744 /usr/local/bin/servico.sh
+
+#Adicionando serviço
+printf "[Unit]\nAfter=network.target\n[Service]\nExecStart=/usr/local/bin/servico.sh\n[Install]\nWantedBy=default.target\n" > /etc/systemd/system/relatorio.service
+
+chmod 664 /etc/systemd/system/relatorio.service
+
+
 #Reinicializando apache2
 systemctl restart apache2
--- atividades/atividade15/servico.sh
+++ atividades/atividade15/servico.sh
@@ -3,7 +3,6 @@ while true
 do
 
     DATA_NOW=$(date +%H:%M:%S-%D)
-
     DATA_ATIVA=$(uptime -p)
 
     CARGA_MEDIA=$(uptime | sed 's/ * / /g' | cut -d" " -f9,10,11)
@@ -11,12 +10,13 @@ do
     MEM_USADA=$(free -h | grep Mem: | sed 's/ * / /g' | cut -d" " -f3)
 
     MEM_LIVRE=$(free -h | grep Mem: | sed 's/ * / /g' | cut -d" " -f4)
-
     BYTES_RECEBIDOS=$(cat /proc/net/dev | grep eth0: | sed 's/ * / /g' | cut -d" " -f3)
 
     BYTES_TRANSMITIDOS=$(cat /proc/net/dev | grep eth0: | sed 's/ * / /g' | cut -d" " -f11)
 
-    INSERT=$(echo "<li>Data: $DATA_NOW Ativa: $DATA_ATIVA | Carga Média: $CARGA_MEDIA | Memória Usada: $MEM_USADA | Memória Livre: $MEM_LIVRE | Bytes Recebidos: $BYTES_RECEBIDOS | Bytes Transmitidos: $BYTES_TRANSMITIDOS</li>)   
+    INSERT=$(echo "Data: $DATA_NOW Ativa: $DATA_ATIVA | Carga Média: $CARGA_MEDIA | Memória Usada: $MEM_USADA | Memória Livre: $MEM_LIVRE | Bytes Recebidos: $BYTES_RECEBIDOS | Bytes Transmitidos: $BYTES_TRANSMITIDOS)
+    #Guardando dados 
+    echo $INSERT >> /tmp/relatorio_serviço.txt
 
     sleep 5
 done
