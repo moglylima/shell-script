5ff9cbd2b3ab2f1580ce3ae904d9b2f743f1e247
Mogleson Lima <moglesonlima@alu.ufc.br>
2021-03-17 15:01:20 -0300

    Sincronizando, aplicando alguns ajustes

--- atividades/atividade14/servidorWeb.sh
+++ atividades/atividade14/servidorWeb.sh
@@ -1,10 +1,13 @@
 #!/bin/bash
-
 echo "Criando Servidor..."
+
+#Chave
+CHAVE=$1
 
 #Variáveis fixas
 IMAGEM_AMI="ami-042e8287309f5df03"
 TIPO_INSTANCIA="t2.micro"
+
 
 #Identificando Subnet
 SUBRED_ID=$(aws ec2 describe-subnets --query "Subnets[0].SubnetId" --output text)
@@ -16,21 +19,24 @@ GRUPO=$(aws ec2 create-security-group --group-name "grupo01" --description "grup
 aws ec2 authorize-security-group-ingress --group-id $GRUPO --protocol tcp --port 22 --cidr 0.0.0.0/0
 aws ec2 authorize-security-group-ingress --group-id $GRUPO --protocol tcp --port 80 --cidr 0.0.0.0/0
 
-
 #Criando instancia
-aws ec2 run-instances --image-id $IMAGEM_AMI --instance-type $TIPO_INSTANCIA --security-group-ids $GRUPO --subnet-id $SUBRED_ID --user-data file://confg_serv.sh > /dev/null
+ID_INSTANCIA=$(aws ec2 run-instances --image-id $IMAGEM_AMI --instance-type $TIPO_INSTANCIA --security-group-ids $GRUPO --subnet-id $SUBRED_ID --key-name $CHAVE --user-data file://confg_serv.sh --query "Instances[0].InstanceId" --output text)
 
+#Verificando status da instancia
+#STATE=$(aws ec2 describe-instances --query "Reservations[0].Instances[0].State.Name" --output text)
 
-#Recuperando IP publico
-IP_PUBLICO=$(aws ec2 describe-instances --query "Reservations[0].Instances[0].PublicIpAddress" --output text)
+STATE=$(aws ec2 describe-instances --instance-id $ID_INSTANCIA --query "Reservations[0].Instances[].State.Name" --output text)
 
-
-#Verificando se o Ip publico já está disponível
-while [ $IP_PUBLICO = "None" ]
+while [ $STATE != "running"  ]
 do
 	sleep 10
-	IP_PUBLICO=$(aws ec2 describe-instances --query "Reservations[0].Instances[0].PublicIpAddress" --output text)
+	STATE=$(aws ec2 describe-instances --instance-id $ID_INSTANCIA --query "Reservations[0].Instances[].State.Name" --output text)
+
 done
+
+#Recuperando IP publico
+IP_PUBLICO=$(aws ec2 describe-instances --instance-id $ID_INSTANCIA --query "Reservations[0].Instances[].PublicIpAddress" --output text)
 
 #Mostrando endereço do servidor
 echo "Acesse -> http://$IP_PUBLICO/"
+
